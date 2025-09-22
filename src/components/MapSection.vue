aefda439ae830ef4db37d49237302254d85fd55c
mateoHinc <mateohincapie219600@correo.itm.edu.co>
2025-09-22 15:26:31 -0500

    Component MapSection.vue, done!

---
+++ src/components/MapSection.vue
@@ -0,0 +1,74 @@
+<template>
+  <section id="escenarios" class="py-5">
+    <div class="container">
+      <div class="align-items-center row g-4">
+        <div class="col-lg-6">
+          <h2 class="section-title fw-bold">Escenarios</h2>
+          <p class="text-body-secondary">
+            Las actividades se concentran en la Zona Norte: Jardín Botánico,
+            Parque Explora y el Parque de los Deseos. Todos los espacios cuentan
+            con ingreso libre hasta completar aforo.
+          </p>
+          <ul class="list-group list-group-flush">
+            <li class="list-group-item d-flex align-items-start gap-3">
+              <i class="text-success bi bi-flower2 fs-4" aria-hidden="true"></i>
+              <div>
+                <strong>Jardín Botánico de Medellín</strong><br /><span
+                  class="text-body-secondary"
+                  >Cra. 52 #73 - 298 · Salas de Lectura y pabellones</span
+                >
+              </div>
+            </li>
+            <li class="list-group-item d-flex align-items-start gap-3">
+              <i
+                class="text-danger bi bi-lightning-charge fs-4"
+                aria-hidden="true"
+              ></i>
+              <div>
+                <strong>Parque Explora</strong><br /><span
+                  class="text-body-secondary"
+                  >Conferencias y Experiencieas interactivas</span
+                >
+              </div>
+            </li>
+            <li class="list-group-item d-flex align-items-start gap-3">
+              <i
+                class="text-danger bi bi-lightning-charge fs-4"
+                aria-hidden="true"
+              ></i>
+              <div>
+                <strong>Parque de los Deseos</strong><br /><span
+                  class="text-body-secondary"
+                  >Cine al aire libre y conciertos</span
+                >
+              </div>
+            </li>
+          </ul>
+        </div>
+        <div class="col-lg-6">
+          <div class="shadow-sm rounded-4 overflow-hidden ratio ratio-4x3">
+            <!-- <iframe
+              title="Mapa Zona Norte Medellín"
+              src="https://www.openstreetmap.org/export/embed.html?bbox=-75.5716%2C6.2673%2C-75.5592%2C6.2806&amp;layer=mapnik"
+              style="border: 0"
+            ></iframe> -->
+            <iframe
+              src="https://www.google.com/maps/embed?pb=!1m16!1m12!1m3!1d7931.898212625101!2d-75.57027775873166!3d6.270423624873076!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!2m1!1sParque%20explora%2C%20parque%20de%20los%20deseos%20y%20jard%C3%ADn%20bot%C3%A1nico!5e0!3m2!1ses-419!2sco!4v1758572576203!5m2!1ses-419!2sco"
+              style="border: 0"
+              title="Mapa Zona Norte Medellín"
+            ></iframe>
+          </div>
+          <small class="mt-2 text-body-secondary d-blod">
+            Mapa referencial de la zona del festival.
+          </small>
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
