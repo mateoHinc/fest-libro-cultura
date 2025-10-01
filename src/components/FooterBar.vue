354e3ecb23539a42e282fcf03def41e2af85f926
mateoHinc <mateohincapie219600@correo.itm.edu.co>
2025-10-01 15:19:31 -0500

    Create component Footer.vue

---
+++ src/components/FooterBar.vue
@@ -0,0 +1,25 @@
+<template>
+  <footer class="py-4 border-top">
+    <div class="d-flex flex-wrap align-items-center gap-3 container">
+      <div class="me-auto">
+        © {{ new Date().getFullYear() }} Festival del libro y la Cultura -
+        Medellín
+      </div>
+      <div class="d-flex gap-3">
+        <a href="#" aria-label="Instagram" class="textbody"
+          ><i class="bi bi-instagram fs-5"></i
+        ></a>
+        <a href="#" aria-label="Facebook" class="textbody"
+          ><i class="bi bi-facebook fs-5"></i
+        ></a>
+        <a href="#" aria-label="X" class="textbody"
+          ><i class="bi bi-twitter-x fs-5"></i
+        ></a>
+        <a href="#" aria-label="Youtube" class="textbody"
+          ><i class="bi bi-youtube fs-5"></i
+        ></a>
+      </div>
+    </div>
+  </footer>
+</template>
+<script setup></script>
